ae9f7ae64cd1ef5a5d12171b82a64e0a660638a5
Joshua Scoggins <Theoretically.x64@gmail.com>
2015-01-18 22:36:38 -0800

    The content on the front page is now printed out
    - The format is:
    	<name> - { <tags> } - <url>
    - Using grep you can select the content you are looking for :D

--- cmd/fakku-content/fakku-content.go
+++ cmd/fakku-content/fakku-content.go
@@ -7,20 +7,34 @@ import (
 )
 
 func main() {
+	//TODO: add support for grabbing beyond the first page
+	//TODO: add support for spitting out CLIPS style facts (template assertions
+	//or instances)
 	posts, err := fakku.GetFrontPage()
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("Something bad happened! Perhaps Fakku is down?")
 	}
-	fmt.Printf("Front page has %d posts on it\n", posts.Total)
 	for i := 0; i < int(posts.Total); i++ {
 		tmp := posts.Index[i]
-		switch t := tmp.(type) {
-		default:
-			fmt.Printf("Unknown entry type %T provided!\n", t)
+		switch tmp.(type) {
 		case fakku.Content:
 			content := tmp.(fakku.Content)
-			fmt.Printf("Content: %s\n", content.Name)
+			tags := content.Tags
+			fmt.Printf("%s - ", content.Name)
+			// print out the tags one after another in a form that can be easily
+			// grepped through
+			if len(tags) == 0 {
+				fmt.Printf("No tags!")
+			} else {
+				fmt.Printf("{ %s", tags[0].Attribute)
+				for j := 1; j < len(tags); j++ {
+					fmt.Printf(", %s", tags[j].Attribute)
+				}
+				fmt.Printf(" }")
+			}
+			fmt.Printf(" - %s", content.Url)
+			fmt.Println()
 		}
 	}
 }
